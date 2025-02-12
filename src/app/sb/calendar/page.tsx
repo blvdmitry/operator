f3afb4774957ced9b0f68bac7f2dadda1caec0d6
Dmitry Belyaev <blv.dmitry@gmail.com>
2025-02-12 17:29:20 +0100

    foo

--- src/app/sb/calendar/page.tsx
+++ src/app/sb/calendar/page.tsx
@@ -6,7 +6,7 @@ import theme from "@bookingcom/bui-react/themes/traveller";
 export default function Home() {
   return (
     <BUIProvider theme={theme}>
-      <Stack>
+      <Stack direction="row">
         <Box mixin={{ width: "300px" }}>
           Accommodation calendar
           <Calendar
@@ -68,6 +68,42 @@ export default function Home() {
         <Box mixin={{ width: "500px" }}>
           Attractions calendar
           <Calendar
+            dayNames={{
+              monday: "Mon",
+              tuesday: "Tue",
+              wednesday: "Wed",
+              thursday: "Thu",
+              friday: "Fri",
+              saturday: "Sat",
+              sunday: "Sun",
+            }}
+            monthNames={{
+              january: "January",
+              february: "February",
+              march: "March",
+              april: "April",
+              may: "May",
+              june: "June",
+              july: "July",
+              august: "August",
+              september: "September",
+              october: "October",
+              november: "November",
+              december: "December",
+            }}
+          />
+        </Box>
+      </Stack>
+      <Stack direction="row">
+        <style
+          dangerouslySetInnerHTML={{
+            __html: ".foo td > span:hover { border: 2px solid }",
+          }}
+        />
+        <Box mixin={{ width: "400px" }}>
+          Ideal calendar
+          <Calendar
+            className="foo"
             dayNames={{
               monday: "Mon",
               tuesday: "Tue",

1630561fbf8603c95ec91d2a1c17ddc513b6d9fb
Dmitry Belyaev <blv.dmitry@gmail.com>
2025-02-12 12:52:59 +0100

    foo

---
+++ src/app/a11y/button-misleading-icon-result/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import {
+  Button,
+  BUIProvider,
+  Box,
+  Stack,
+  Calendar,
+} from "@bookingcom/bui-react";
+import theme from "@bookingcom/bui-react/themes/traveller";
+import { useState } from "react";
+import { Calendar1, Popcorn } from "lucide-react";
+
+export default function Home() {
+  const [active, setActive] = useState(false);
+
+  return (
+    <BUIProvider theme={theme}>
+      <Box>
+        <Stack direction="row">
+          <Button>Button</Button>
+          <Button
+            onClick={() => setActive((prev) => !prev)}
+            icon={<Popcorn />}
+          />
+          <Button
+            onClick={() => setActive((prev) => !prev)}
+            icon={<Calendar1 />}
+          />
+          <Button>Button</Button>
+        </Stack>
+      </Box>
+      {active && (
+        <Box mixin={{ width: "400px" }}>
+          <Calendar
+            dayNames={{
+              monday: "Mon",
+              tuesday: "Tue",
+              wednesday: "Wed",
+              thursday: "Thu",
+              friday: "Fri",
+              saturday: "Sat",
+              sunday: "Sun",
+            }}
+            monthNames={{
+              january: "January",
+              february: "February",
+              march: "March",
+              april: "April",
+              may: "May",
+              june: "June",
+              july: "July",
+              august: "August",
+              september: "September",
+              october: "October",
+              november: "November",
+              december: "December",
+            }}
+          />
+        </Box>
+      )}
+    </BUIProvider>
+  );
+}

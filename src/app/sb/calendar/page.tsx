6ff03f522707097167a216a61c18685163fe6b99
Dmitry Belyaev <blv.dmitry@gmail.com>
2025-02-12 16:53:50 +0100

    foo

--- src/app/sb/calendar/page.tsx
+++ src/app/sb/calendar/page.tsx
@@ -1,38 +1,99 @@
 "use client";
 
-import { BUIProvider, Box, Calendar } from "@bookingcom/bui-react";
+import { BUIProvider, Box, Calendar, Stack } from "@bookingcom/bui-react";
 import theme from "@bookingcom/bui-react/themes/traveller";
 
 export default function Home() {
   return (
     <BUIProvider theme={theme}>
-      <Box mixin={{ width: "400px" }}>
-        <Calendar
-          dayNames={{
-            monday: "Mon",
-            tuesday: "Tue",
-            wednesday: "Wed",
-            thursday: "Thu",
-            friday: "Fri",
-            saturday: "Sat",
-            sunday: "Sun",
-          }}
-          monthNames={{
-            january: "January",
-            february: "February",
-            march: "March",
-            april: "April",
-            may: "May",
-            june: "June",
-            july: "July",
-            august: "August",
-            september: "September",
-            october: "October",
-            november: "November",
-            december: "December",
-          }}
-        />
-      </Box>
+      <Stack>
+        <Box mixin={{ width: "300px" }}>
+          Accommodation calendar
+          <Calendar
+            dayNames={{
+              monday: "Mon",
+              tuesday: "Tue",
+              wednesday: "Wed",
+              thursday: "Thu",
+              friday: "Fri",
+              saturday: "Sat",
+              sunday: "Sun",
+            }}
+            monthNames={{
+              january: "January",
+              february: "February",
+              march: "March",
+              april: "April",
+              may: "May",
+              june: "June",
+              july: "July",
+              august: "August",
+              september: "September",
+              october: "October",
+              november: "November",
+              december: "December",
+            }}
+          />
+        </Box>
+
+        <Box mixin={{ width: "400px" }}>
+          Flights calendar
+          <Calendar
+            dayNames={{
+              monday: "Mon",
+              tuesday: "Tue",
+              wednesday: "Wed",
+              thursday: "Thu",
+              friday: "Fri",
+              saturday: "Sat",
+              sunday: "Sun",
+            }}
+            monthNames={{
+              january: "January",
+              february: "February",
+              march: "March",
+              april: "April",
+              may: "May",
+              june: "June",
+              july: "July",
+              august: "August",
+              september: "September",
+              october: "October",
+              november: "November",
+              december: "December",
+            }}
+          />
+        </Box>
+
+        <Box mixin={{ width: "500px" }}>
+          Attractions calendar
+          <Calendar
+            dayNames={{
+              monday: "Mon",
+              tuesday: "Tue",
+              wednesday: "Wed",
+              thursday: "Thu",
+              friday: "Fri",
+              saturday: "Sat",
+              sunday: "Sun",
+            }}
+            monthNames={{
+              january: "January",
+              february: "February",
+              march: "March",
+              april: "April",
+              may: "May",
+              june: "June",
+              july: "July",
+              august: "August",
+              september: "September",
+              october: "October",
+              november: "November",
+              december: "December",
+            }}
+          />
+        </Box>
+      </Stack>
     </BUIProvider>
   );
 }
